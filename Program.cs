f3e805aff40f1e1be1b877a0ad08099ad68a6e44
Tatsuya Sasajima <committer@sasaji.local>
2020-07-11 15:23:17 +0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace LogOnAsAService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string accountName in args) {
+                LsaUtility.SetRight(accountName, "SeServiceLogonRight");
+            }
+        }
+    }
+}
